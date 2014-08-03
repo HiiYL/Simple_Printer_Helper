c22cad3e3cfdb7f64fb8fc265b40c6b191db31d3
YLHii <yonglian146@gmail.com>
2014-08-03 21:49:09 +0800

    Simple tool for printing double sided pages using a single sided printer

---
+++ Printer Helper.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    vector<int> odd, even;
+    string file_name("output.txt");
+    ofstream print(file_name);
+    int page, slides_per_page;
+    int print_page = 0;
+    cout << "How many slides does the document have?"
+         << ">>";
+    cin >> page;
+    cout << "How many slides per page?"
+         << ">>";
+    cin >> slides_per_page;
+
+    int j = 1;
+    while(j<page)   {
+    for(int i = 0;i < slides_per_page && j < page;i++,j++)
+        odd.push_back(j);
+    for(int i = 0;i < slides_per_page && j < page;i++,j++)
+        even.push_back(j);
+    print_page++;
+    }
+    print << "ODD"<<endl;
+    for (auto &i : odd)
+        print << i <<",";
+    print << endl;
+    print << "EVEN" <<endl;
+    for(auto &j : even)
+        print << j <<",";
+    print << endl << "Sheets of Paper needed :" << print_page <<endl;
+    cout << "Results printed to file named " << file_name <<endl;
+    system("notepad.exe output.txt");
+    return 0;
+
+}
